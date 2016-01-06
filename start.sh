11d3d7e5969af2b22854eb54b052310bb9622822
David Bell <dave@evad.info>
2016-01-06 17:32:18 +0000

    Delete start.sh

--- start.sh
+++
@@ -1,1 +0,0 @@
-/usr/bin/gunicorn --bind 0.0.0.0:5000 --certfile /etc/puppetlabs/puppet/ssl/ca/signed/poc.puppet.soton.ac.uk.pem --keyfile /etc/puppetlabs/puppet/ssl/private_keys/poc.puppet.soton.ac.uk.pem  app:app
